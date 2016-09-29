66547833f7dde589fd2fb1bb039c955998058d30
DHLife <dhlife09@gmail.com>
2016-09-29 18:55:15 +0900

    Add files via upload

--- SpawnPoint.cs
+++ SpawnPoint.cs
@@ -1,14 +1,23 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class SpawnPoint : MonoBehaviour {
 	public GameObject debri; //우주쓰레기 프레펩
 	public float interval=1f; //쓰레기 발생 간격(시간)
+	private bool spawnStarted=false; //쓰레기가 발생중인지 체크하는 플래그변수
 	// Use this for initialization
-	void Start () {
-		StartCoroutine ("SpawnDebri"); //코루틴의 시작
-	}
-	
+	void StartSpawn () {
+				if (!spawnStarted) {
+					spawnStarted = true;
+					StartCoroutine ("SpawnDebri"); //코루틴의 시작
+				}
+		}
+	void StopSpawn(){
+				if (spawnStarted) {
+					spawnStarted = false;
+					StopCoroutine ("SpawnDebri"); //코루틴 정지
+				}
+		}
 	// Update is called once per frame
 	void Update () {
 	
@@ -20,5 +29,5 @@ public class SpawnPoint : MonoBehaviour {
             //Instantiate(원본, 좌표, 각도);
 			yield return new WaitForSeconds (interval); //interval 만큼 기다림
 		}
-	}
+	}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 }
