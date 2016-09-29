bd0fbe5b493854c6dda29cbbc025a2d818c55fa5
DHLife <dhlife09@gmail.com>
2016-09-29 18:53:26 +0900

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	private int score;
+	// Use this for initialization
+	void InitScore () {
+		this.score = 0;
+	}
+
+	void AddScore(){
+		this.score += score;
+	}
+	// Update is called once per frame
+	void Update () {
+		guiText.text = "Score : " + this.score;
+	}
+}
